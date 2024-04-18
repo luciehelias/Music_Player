6e93c8e6a4b29438b77f626da4e377e0b2371a88
luciehelias <lucie.helias@protonmail.com>
2024-04-18 15:41:02 +0200

    finish the project

--- script.js
+++ script.js
@@ -52,7 +52,31 @@ function prevSong() {
   playSong();
 }
 
-function nextSong() {}
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
 
 // Event listeners
 playBtn.addEventListener('click', () => {
@@ -68,3 +92,8 @@ playBtn.addEventListener('click', () => {
 // change song events
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
